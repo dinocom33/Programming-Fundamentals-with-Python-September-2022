99e19074d889142820d79a26304b3a2aa3304112
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-09-28 10:40:49 +0300

    Change code

--- 2-Data-Types-and-Variables/1-Lab/next_happy_year.py
+++ 2-Data-Types-and-Variables/1-Lab/next_happy_year.py
@@ -4,9 +4,10 @@ is_happy_year = False
 
 while not is_happy_year:
     year += 1
-    year_as_string = str(year)
-    if year_as_string[0] != year_as_string[1] and year_as_string[0] != year_as_string[2] and \
-            year_as_string[0] != year_as_string[3] and year_as_string[1] != year_as_string[2] and \
-            year_as_string[1] != year_as_string[3] and year_as_string[2] != year_as_string[3]:
-        is_happy_year = True
+    year_as_set = set()
+    for current_digit in range(len(str(year))):
+        year_as_set.add(str(year)[current_digit])
+        if len(year_as_set) == len(str(year)):
+            is_happy_year = True
+            break
 print(year)
