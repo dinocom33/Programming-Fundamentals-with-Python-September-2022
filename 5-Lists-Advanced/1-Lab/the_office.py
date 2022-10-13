efb7ccda0bb7b34b7bf27edc13d29f58530a796d
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-10-13 13:05:22 +0300

    Update the_office.py

--- 5-Lists-Advanced/1-Lab/the_office.py
+++ 5-Lists-Advanced/1-Lab/the_office.py
@@ -8,3 +8,4 @@ if len(happy_employees) >= len(current_happiness) / 2:
     print(f"Score: {len(happy_employees)}/{len(current_happiness)}. Employees are happy!")
 else:
     print(f"Score: {len(happy_employees)}/{len(current_happiness)}. Employees are not happy!")
+
