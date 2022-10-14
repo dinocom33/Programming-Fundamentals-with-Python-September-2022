06b957d2a80f2ce2a1a2811c0086cc29cc830a09
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-10-14 12:20:49 +0300

    mid exams

--- Mid-Exams/numbers.py
+++ Mid-Exams/numbers.py
@@ -3,4 +3,11 @@ numbers = list(map(int, input().split()))
 numbers_list = numbers.copy()
 average_value = sum(numbers_list) / len(numbers_list)
 
-print()
+greater_numbers = sorted([num for num in numbers_list if num > average_value], reverse=True)
+
+if len(greater_numbers) > 5:
+    print(*greater_numbers[0:5], sep=" ")
+elif len(greater_numbers) < 1:
+    print("No")
+else:
+    print(*greater_numbers, sep=" ")
