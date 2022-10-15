ec6c47e13384db7873c8f6a6b2ebb35222770cb3
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-10-15 13:46:50 +0300

    Update group_of_10s.py

--- 5-Lists-Advanced/2-Exercises/group_of_10s.py
+++ 5-Lists-Advanced/2-Exercises/group_of_10s.py
@@ -3,7 +3,7 @@ numbers = list(map(int, input().split(", ")))
 boundary = 10
 
 while len(numbers) > 0:
-    filtered = list(filter(lambda x: x in range(boundary - 9, boundary + 1), numbers))
+    filtered = list(filter(lambda x: x <= boundary, numbers))
     numbers = [i for i in numbers if i not in filtered]
     print(f"Group of {boundary}'s: {filtered}")
     boundary += 10
