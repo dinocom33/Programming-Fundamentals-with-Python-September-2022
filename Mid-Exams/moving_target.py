bff4e6faba8d9caed1a862bcb9f76797dcd02384
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-10-20 13:17:21 +0300

    Update moving_target.py

--- Mid-Exams/moving_target.py
+++ Mid-Exams/moving_target.py
@@ -6,31 +6,28 @@ while command != "End":
     current_command = command.split(" ")
     action = current_command[0]
     index = int(current_command[1])
-    if action == "Shoot":
-        if 0 <= index < len(targets):
-            power = int(current_command[2])
-            if targets[index] - power <= 0:
-                targets.pop(index)
-            else:
-                targets[index] -= power
+    value = int(current_command[2])
+
+    if action == "Shoot" and 0 <= index < len(targets):
+        current_target = targets[index]
+        current_target -= value
+        if current_target <= 0:
+            targets.pop(index)
+        else:
+            targets[index] = current_target
 
     elif action == "Add":
         if 0 <= index < len(targets):
-            value = current_command[2]
             targets.insert(index, value)
         else:
             print("Invalid placement!")
-            
+
     elif action == "Strike":
-        radius = int(current_command[2])
-        index_validator = index - radius >= 0 and index + radius < len(targets)
-        if index_validator:
-            start_index = index - radius
-            end_index = index + radius
-            targets = [targets[i] for i in range(len(targets)) if i < start_index or i > end_index]
+        if index - value >= 0 and index + value < len(targets):
+            targets = targets[:index - value] + targets[index + value + 1:]
         else:
             print("Strike missed!")
 
     command = input()
-
-print(*targets, sep="|")
+output = list(map(str, targets))
+print("|".join(output))
