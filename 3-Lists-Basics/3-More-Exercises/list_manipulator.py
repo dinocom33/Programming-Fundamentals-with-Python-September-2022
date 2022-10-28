8846e3716eaf8b31b11250e68b44d1e73b115a43
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-10-28 18:07:59 +0300

    Update list_manipulator.py

--- 3-Lists-Basics/3-More-Exercises/list_manipulator.py
+++ 3-Lists-Basics/3-More-Exercises/list_manipulator.py
@@ -1,6 +1,5 @@
 initial_list = list(map(int, input().split(" ")))
 
-temp_list = initial_list.copy()
 even_numbers = []
 odd_numbers = []
 
@@ -32,9 +31,11 @@ while command != "end":
     elif action == "min":
         value = current_command[1]
         if value == "even" and even_numbers:
-            print((len(initial_list) - initial_list[::-1].index(min(even_numbers)) - 1))
+            result = (len(initial_list) - initial_list[::-1].index(min(even_numbers)) - 1)
+            print(result)
         elif value == "odd":
-            print((len(initial_list) - initial_list[::-1].index(min(odd_numbers)) - 1))
+            result = (len(initial_list) - initial_list[::-1].index(min(odd_numbers)) - 1)
+            print(result)
         else:
             print("No matches")
 
