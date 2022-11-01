d0948fb240017a49a905e64ff7b259e1c6bf0bf1
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-11-01 10:41:09 +0200

    Exercises

---
+++ 6-Classes-and-Objects/2-Exercises/vehicle.py
@@ -0,0 +1,40 @@
+class Vehicle:
+
+    def __init__(self, type, model, price, owner=None):
+        self.type = type
+        self.model = model
+        self.price = price
+        self.owner = owner
+
+    def buy(self, money: int, owner: str):
+        if money >= self.price and self.owner is None:
+            change = money - self.price
+            self.owner = owner
+            return f"Successfully bought a {self.type}. Change: {change:.2f}"
+        elif money < self.price:
+            return f"Sorry, not enough money"
+        else:
+            return "Car already sold"
+
+    def sell(self):
+        if self.owner is not None:
+            self.owner = None
+        else:
+            return f"Vehicle has no owner"
+
+    def __repr__(self):
+        if self.owner:
+            return f"{self.model} {self.type} is owned by: {self.owner}"
+        else:
+            return f"{self.model} {self.type} is on sale: {self.price}"
+
+
+vehicle_type = "car"
+model = "BMW"
+price = 30000
+vehicle = Vehicle(vehicle_type, model, price)
+print(vehicle.buy(15000, "Peter"))
+print(vehicle.buy(35000, "George"))
+print(vehicle)
+vehicle.sell()
+print(vehicle)
