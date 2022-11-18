62323efb5431bae8dc3ce6e38bca179efe4e8898
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-11-18 13:46:24 +0200

    Update match_dates.py

--- Regular-Expressions/1-Lab/match_dates.py
+++ Regular-Expressions/1-Lab/match_dates.py
@@ -2,7 +2,7 @@ import re
 
 dates = input()
 
-search_pattern = r"\b(\d{2})([.\-/])([A-Z][a-z]{2})\2(\d{4}\b)"
+search_pattern = r"\b[a-z]+[\w.-]+@[A-Za-z0-9-]+\.[A-Z|a-z.]{2,}\b"
 
 result = re.findall(search_pattern, dates)
 
