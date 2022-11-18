9306d61506b5aca560388d9a06beeefc50d88209
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-11-18 11:30:00 +0200

    Update extract_emails.py

--- Regular-Expressions/2-Exercises/extract_emails.py
+++ Regular-Expressions/2-Exercises/extract_emails.py
@@ -2,7 +2,7 @@ import re
 
 emails = input()
 
-searches_pattern = re.compile(r"[a-zA-Z.-]+@[a-z-]+\.?[a-z.-]{2,}\b")
+searches_pattern = r"[a-zA-Z.-]+@[a-z-]+\.?[a-z.-]{2,}\b"
 
 result = re.finditer(searches_pattern, emails)
 
