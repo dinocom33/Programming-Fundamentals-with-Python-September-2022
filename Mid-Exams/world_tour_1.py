b0ecd909d922695b6844087b7f7a33ba1b819244
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-11-25 18:48:31 +0200

    Update world_tour_1.py

--- Mid-Exams/world_tour_1.py
+++ Mid-Exams/world_tour_1.py
@@ -1,5 +1,5 @@
-def valid_index(index):
-    if 0 <= index < len(all_stops):
+def valid_index(index_to_validate):
+    if 0 <= index_to_validate < len(all_stops):
         return True
     return False
 
@@ -25,22 +25,22 @@ def switch_stops(stops, old, new):
 all_stops = input()
 
 while True:
-    command = input()
-    if command == "Travel":
+    command = input().split(":")
+    action = command[0]
+    if action == "Travel":
         break
-    current_command = command.split(":")
-    action = current_command[0]
+
     if action == "Add Stop":
-        index = int(current_command[1])
-        string_to_add = current_command[2]
+        index = int(command[1])
+        string_to_add = command[2]
         all_stops = add_stop(all_stops, index, string_to_add)
     elif action == "Remove Stop":
-        start_index = int(current_command[1])
-        end_index = int(current_command[2])
+        start_index = int(command[1])
+        end_index = int(command[2])
         all_stops = remove_stop(all_stops, start_index, end_index)
     elif action == "Switch":
-        old_string = current_command[1]
-        new_string = current_command[2]
+        old_string = command[1]
+        new_string = command[2]
         all_stops = switch_stops(all_stops, old_string, new_string)
     print(all_stops)
 
